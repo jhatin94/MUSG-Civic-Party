e580b6811f3b35f926eca478bfdfc8b755e9c25b
Justin Hatin <JHatin94@gmail.com>
2016-07-10 23:43:12 -0400

    Site v0.0.06
    Updated MVP structure with basic files

--- Scripts/homePagePresenter.js
+++ Scripts/homePagePresenter.js
@@ -1,31 +1,21 @@
-// Home Page Presenter
-	
-var HomePagePresenter = (function() {
-	/*public properties*/
-	/*end public properties*/
-	
-	/*private properties*/
-	/*end private properties*/
-	
-	/*public functions*/
-	function DOMReady(callback) {
-        if (document.readyState === 'interactive' || document.readyState === 'complete') {
-            return callback();
-        }
-        document.addEventListener('DOMContentLoaded', callback, false);
-    }
-	function renderPage() {
-		
-	}
-	/*end public functions*/
-	
-	/*private functions*/
-	/*end private functions*/
-	
-	return {
-		DOMReady: DOMReady,
-		renderPage: renderPage
-	}
-})();
+// renders homepage and handles activity there
 
-window.onload = HomePagePresenter.DOMReady(function () {HomePagePresenter.renderPage();});
+var HomePagePresenter = (function () {
+    /*public properties*/
+    /*end public properties*/
+
+    /*private properties*/
+    /*end private properties*/
+
+    /*public functions*/
+    function renderPage() {
+        Main.showPage(Main.pageContainers.homePageContainer);
+    }
+    /*end public functions*/
+
+    /*private properties*/
+    /*end private functions*/
+    return {
+        renderPage: renderPage
+    }
+})();
