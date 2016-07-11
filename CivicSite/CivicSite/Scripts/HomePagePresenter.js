91bbd6ca049fb729101755f761e73cdb37714463
Justin Hatin <JHatin94@gmail.com>
2016-07-10 23:11:20 -0400

    Site v0.0.03
    MVC/MVP Project from Visual Studio initial commit

---
+++ CivicSite/CivicSite/Scripts/HomePagePresenter.js
@@ -0,0 +1,21 @@
+﻿// renders homepage and handles activity there
+
+var HomePagePresenter = (function () {
+    /*public properties*/
+    /*end public properties*/
+
+    /*private properties*/
+    /*end private properties*/
+
+    /*public functions*/
+    function renderPage() {
+        Main.showPage(Main.pageContainers.homePageContainer);
+    }
+    /*end public functions*/
+
+    /*private properties*/
+    /*end private functions*/
+    return {
+        renderPage: renderPage
+    }
+})();
